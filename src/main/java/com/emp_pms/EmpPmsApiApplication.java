26692bf08af7b6dcb72beb334570acd7f2e44f5a
bhadange2000 <dbhadange2018@gmail.com>
2023-11-28 09:31:41 +0530

    initial commit

---
+++ src/main/java/com/emp_pms/EmpPmsApiApplication.java
@@ -0,0 +1,14 @@
+package com.emp_pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpPmsApiApplication.class, args);
+		System.out.println("Employee management prduct Start Successfully;");
+	}
+
+}
---
+++ src/test/java/com/emp_pms/EmpPmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp_pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpPmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
